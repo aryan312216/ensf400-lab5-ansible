4a0cfe9159689e5d0f3250423dae25fce54120c1
aryan312216 <113812402+aryan312216@users.noreply.github.com>
2024-03-24 04:26:05 +0000

    update

--- load_inventory.py
+++ load_inventory.py
@@ -1,19 +1,23 @@
 from ansible.parsing.dataloader import DataLoader
 from ansible.inventory.manager import InventoryManager
+from ansible.vars.manager import VariableManager
+from ansible.executor.task_queue_manager import TaskQueueManager
+from ansible.playbook.play import Play
 
 # Initialize a DataLoader
 loader = DataLoader()
 
 # Initialize an InventoryManager with the DataLoader
-inventory = InventoryManager(loader=loader, sources='/workspaces/ensf400-lab5-ansible/load_inventory.py')
+inventory = InventoryManager(loader=loader, sources='hosts.yml')
+
+# Initialize a VariableManager
+variable_manager = VariableManager(loader=loader, inventory=inventory)
 
 # Print the names, IP addresses, and group names of all hosts
 for host in inventory.get_hosts():
     print(f"Name: {host.name}, IP: {host.vars['ansible_host']}, Groups: {[g.name for g in host.groups]}")
 
 # Ping all hosts
-from ansible.executor.task_queue_manager import TaskQueueManager
-from ansible.playbook.play import Play
 
 # Create a play
 play_source = dict(
@@ -29,7 +33,8 @@ play_source = dict(
 # Run the play
 tqm = TaskQueueManager(
     inventory=inventory,
+    variable_manager=variable_manager,
     loader=loader,
-    
+    passwords=dict(),
 )
 result = tqm.run(Play().load(play_source, loader=loader))
